9615b7f92a757f201a35673c3d1a28ba38828cf5
Ihor Vansach <ihoronesuch@gmail.com>
2016-02-21 20:16:05 +0200

    Add info blocks

---
+++ app/code/Magefan/Blog/Block/Sidebar/Rss.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © 2016 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Block\Sidebar;
+
+/**
+ * Blog sidebar rss
+ */
+class Rss extends \Magento\Framework\View\Element\Template
+{
+    use Widget;
+
+    /**
+     * @var string
+     */
+    protected $_widgetKey = 'rss_feed';
+
+    /**
+     * Available months
+     * @var array
+     */
+    protected $_months;
+
+	/**
+     * Retrieve blog identities
+     * @return array
+     */
+	public function getIdentities()
+    {
+        return [\Magento\Cms\Model\Block::CACHE_TAG . '_blog_rss_widget'  ];
+    }
+
+}
