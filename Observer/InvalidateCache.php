e00dbce1ad409b13110bda4aee883d1ca2d4439d
Ihor Vansach <ihoronesuch@gmail.com>
2016-03-21 23:45:08 +0200

    No cache invalidation message #14

---
+++ Observer/InvalidateCache.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © 2016 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Blog observer
+ */
+class InvalidateCache implements ObserverInterface
+{
+    /**
+     * @var \Magento\Framework\App\Cache\TypeListInterface
+     */
+    protected $_typeList;
+
+    /**
+     * Application config object
+     *
+     * @var \Magento\PageCache\Model\Config
+     */
+    protected $_config;
+
+    /**
+     * @param \Magento\PageCache\Model\Config $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface $typeList
+     */
+    public function __construct(
+        \Magento\PageCache\Model\Config $config,
+        \Magento\Framework\App\Cache\TypeListInterface $typeList
+    ) {
+        $this->_config = $config;
+        $this->_typeList = $typeList;
+    }
+
+    /**
+     * Invalidate full page and block cache
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        if ($this->_config->isEnabled()) {
+            $this->_typeList->invalidate(
+                \Magento\PageCache\Model\Cache\Type::TYPE_IDENTIFIER
+            );
+        }
+
+        $this->_typeList->invalidate(
+            \Magento\Framework\App\Cache\Type\Block::TYPE_IDENTIFIER
+        );
+    }
+}
