49ece35db49837367e3579452f9964aba4b4be98
Ihor Vansach <ihor@magefan.com>
2016-11-30 20:49:29 +0200

    Fixed issue with aditional body tags and DOMDocument Warning

--- Block/Post/AbstractPost.php
+++ Block/Post/AbstractPost.php
@@ -103,11 +103,21 @@ abstract class AbstractPost extends \Magento\Framework\View\Element\Template
 
         if ($p = mb_strpos($content, $pageBraker)) {
             $content = mb_substr($content, 0, $p);
+            try {
+                libxml_use_internal_errors(true);
+                $dom = new \DOMDocument();
+                $dom->loadHTML('<?xml encoding="UTF-8">' . $content);
+                $body = $dom->getElementsByTagName('body');
+                if ( $body && $body->length > 0 ) {
+                    $body = $body->item(0);
+                    $_content = new \DOMDocument;
+                    foreach ($body->childNodes as $child){
+                        $_content->appendChild($_content->importNode($child, true));
+                    }
+                    $content = $_content->saveHTML();
+                }
+            } catch (\Exception $e) {}
         }
-
-        $dom = new \DOMDocument();
-        $dom->loadHTML('<?xml encoding="UTF-8">' . $content);
-        $content = $dom->saveHTML();
 
         return $content;
     }
--- Block/Widget/Recent.php
+++ Block/Widget/Recent.php
@@ -131,18 +131,28 @@ class Recent extends \Magefan\Blog\Block\Post\PostList\AbstractList implements \
      */
     public function getShorContent($post)
     {
-        $content = $post->getContent();
+        $content = $this->_filterProvider->getPageFilter()->filter(
+            $post->getContent()
+        );
         $pageBraker = '<!-- pagebreak -->';
 
         if ($p = mb_strpos($content, $pageBraker)) {
             $content = mb_substr($content, 0, $p);
+            try {
+                libxml_use_internal_errors(true);
+                $dom = new \DOMDocument();
+                $dom->loadHTML('<?xml encoding="UTF-8">' . $content);
+                $body = $dom->getElementsByTagName('body');
+                if ( $body && $body->length > 0 ) {
+                    $body = $body->item(0);
+                    $_content = new \DOMDocument;
+                    foreach ($body->childNodes as $child){
+                        $_content->appendChild($_content->importNode($child, true));
+                    }
+                    $content = $_content->saveHTML();
+                }
+            } catch (\Exception $e) {}
         }
-
-        $content = $this->_filterProvider->getPageFilter()->filter($content);
-
-        $dom = new \DOMDocument();
-        $dom->loadHTML('<?xml encoding="UTF-8">' . $content);
-        $content = $dom->saveHTML();
 
         return $content;
     }
