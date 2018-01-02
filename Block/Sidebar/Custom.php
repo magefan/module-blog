a2d043a1df24dcb8ee1f8b1abbe2978ab5ee3c72
Ihor Vansach <ihor@magefan.com>
2018-01-02 16:53:04 +0200

    Added 2 custom HTML blocks into blog sidebar

---
+++ Block/Sidebar/Custom.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright © 2015-2018 Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Block\Sidebar;
+
+use \Magento\Store\Model\ScopeInterface;
+/**
+ * Blog sidebar custom block
+ */
+class Custom extends \Magento\Framework\View\Element\Template
+{
+    use Widget;
+
+    /**
+     * @var \Magento\Cms\Model\Template\FilterProvider
+     */
+    protected $filterProvider;
+
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\View\Element\Context $context
+     * @param \Magefan\Blog\Model\Url $url
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Cms\Model\Template\FilterProvider $filterProvider,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->filterProvider = $filterProvider;
+    }
+
+    /**
+     * @var string
+     */
+    protected $_widgetKey = 'custom';
+
+    /**
+     * Retrieve query
+     * @return string
+     */
+    public function getQuery()
+    {
+        return urldecode($this->getRequest()->getParam('q', ''));
+    }
+
+    /**
+     * Retrieve filtered content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        $key = 'content';
+        if (!$this->hasData($key)) {
+
+            $content = $this->_scopeConfig->getValue('mfblog/sidebar/'.$this->_widgetKey.'/html', ScopeInterface::SCOPE_STORE);
+            $content = $this->filterProvider->getPageFilter()->filter(
+                $content
+            );
+            $this->setData($key, $content);
+        }
+        return $this->getData($key);
+    }
+}
---
+++ Block/Sidebar/CustomTwo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © 2015-2018 Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Block\Sidebar;
+
+use \Magento\Store\Model\ScopeInterface;
+/**
+ * Blog sidebar custom block
+ */
+
+class CustomTwo extends Custom
+{
+    /**
+     * @var string
+     */
+    protected $_widgetKey = 'custom2';
+}
