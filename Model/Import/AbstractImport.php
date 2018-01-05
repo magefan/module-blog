5a0ad688f96ba2b53f6ad2392994fe8c34e15c9f
termos008 <34942603+termos008@users.noreply.github.com>
2018-01-05 16:13:19 +0200

    Update AbstractImport.php

--- Model/Import/AbstractImport.php
+++ Model/Import/AbstractImport.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Copyright © 2016 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * Copyright © 2017 Magefan (support@magefan.com). All rights reserved.
  * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
- *
- * Glory to Ukraine! Glory to the heroes!
  */
 
 namespace Magefan\Blog\Model\Import;
