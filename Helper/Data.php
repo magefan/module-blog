30a0722a267420cf7e1dc6fe553ee5b86cac412e
Magefan <ihoronesuch@gmail.com>
2018-01-19 21:18:26 +0200

    Added getConfig method to data helper
    This method  added to fix the issue on stores that use Sm/market theme with an old blog hard-coded version

--- Helper/Data.php
+++ Helper/Data.php
@@ -38,4 +38,17 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 
         return $date;
     }
+    
+    /**
+     * Retrieve store config value
+     * @param  string $path
+     * @return mixed
+     */
+    public function getConfig($path)
+    {
+        return $this->scopeConfig->getValue(
+            $path,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
 }
