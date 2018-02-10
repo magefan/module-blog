75c29453bff07ff22f04c26f60f57d3a6b5220cc
Ihor Vansach <ihor@magefan.com>
2018-02-10 12:56:47 +0200

    Changed copyright

--- view/frontend/web/css/blog-m.css
+++ view/frontend/web/css/blog-m.css
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2015-2017 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
  * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
  *
  * Glory to Ukraine! Glory to the heroes!
