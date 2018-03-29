6d0c6894dc0151d57b1607631229a02b381fc024
Ihor Vansach <ihor@magefan.com>
2018-03-29 23:09:52 +0300

    Add tag dates input filters

--- Controller/Adminhtml/Tag/Save.php
+++ Controller/Adminhtml/Tag/Save.php
@@ -13,5 +13,31 @@ namespace Magefan\Blog\Controller\Adminhtml\Tag;
  */
 class Save extends \Magefan\Blog\Controller\Adminhtml\Tag
 {
+	/**
+     * Filter request params
+     * @param  array $data
+     * @return array
+     */
+    protected function filterParams($data)
+    {
+        /* Prepare dates */
+        $dateFilter = $this->_objectManager->create(\Magento\Framework\Stdlib\DateTime\Filter\Date::class);
 
+        $filterRules = [];
+        foreach (['custom_theme_from', 'custom_theme_to'] as $dateField) {
+            if (!empty($data[$dateField])) {
+                $filterRules[$dateField] = $dateFilter;
+            }
+        }
+
+        $inputFilter = new \Zend_Filter_Input(
+            $filterRules,
+            [],
+            $data
+        );
+
+        $data = $inputFilter->getUnescaped();
+
+        return $data;
+    }
 }
