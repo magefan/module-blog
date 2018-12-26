13bcec91b2ae14bcee36634b0afad4592a25b7b4
bohdan <bohdan.berezhniy@magefan.com>
2018-12-26 17:58:28 +0200

    changed authorfactory on author interfacefactory

---
+++ Api/AuthorInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Api;
+
+interface AuthorInterface
+{
+
+}
