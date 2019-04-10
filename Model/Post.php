6fc75d9054e862d3b7a739bf03df77aad8311b4b
Magefan <ihoronesuch@gmail.com>
2019-04-10 11:45:04 +0300

    unsetData('identifier') on post duplication

--- Model/Post.php
+++ Model/Post.php
@@ -898,6 +898,7 @@ class Post extends \Magento\Framework\Model\AbstractModel implements \Magento\Fr
             ->unsetData('creation_time')
             ->unsetData('update_time')
             ->unsetData('publish_time')
+            ->unsetData('identifier')
             ->setTitle($object->getTitle() . ' (' . __('Duplicated') . ')')
             ->setData('is_active', 0);
 
