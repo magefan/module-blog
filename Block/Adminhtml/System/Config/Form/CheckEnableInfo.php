c24762cda8096c22a07a054c61e5863a516f78fa
dev4 <Yuriy>
2019-05-24 12:31:14 +0300

    2147-check-blog-enable [Implemented]

---
+++ Block/Adminhtml/System/Config/Form/CheckEnableInfo.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Block\Adminhtml\System\Config\Form;
+
+/**
+ * Class CheckEnableInfo
+ * @package Magefan\Blog\Block\Adminhtml\System\Config\Form
+ */
+class CheckEnableInfo extends \Magento\Backend\Block\Template
+{
+    /**
+     * Magefan Blog Plus
+     */
+    const MAGEFAN_BLOG_PLUS = 'Magefan_BlogPlus';
+
+    /**
+     * Extension key config path
+     */
+    const XML_PATH_KEY = 'mfblog/general/key';
+
+    /**
+     * @var \Magefan\Blog\Model\Config
+     */
+    protected $config;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var \Magento\Framework\Module\ModuleListInterface
+     */
+    protected $moduleList;
+
+    /**
+     * @var \Magento\Framework\App\ProductMetadataInterface
+     */
+    protected $metadata;
+
+    /**
+     * CheckEnableInfo constructor.
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magefan\Blog\Model\Config $config
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Module\ModuleListInterface $moduleList
+     * @param \Magento\Framework\App\ProductMetadataInterface $metadata
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magefan\Blog\Model\Config $config,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Framework\App\ProductMetadataInterface $metadata,
+        array $data = []
+    ) {
+        $this->config = $config;
+        $this->_storeManager = $storeManager;
+        $this->moduleList       = $moduleList;
+        $this->metadata = $metadata;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        foreach ($this->_storeManager->getWebsites() as $website) {
+            foreach ($website->getGroups() as $group) {
+                $stores = $group->getStores();
+                if (count($stores) == 0) {
+                    continue;
+                }
+
+                foreach ($stores as $store) {
+                    if ($this->config->isEnabled($store->getId())) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isKeyMissing()
+    {
+        return !$this->config->getConfig(self::XML_PATH_KEY)
+            && ($this->metadata->getEdition() != 'Community'
+                || $this->moduleList->has(self::MAGEFAN_BLOG_PLUS)
+        );
+    }
+}
