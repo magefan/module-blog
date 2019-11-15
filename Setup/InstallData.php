95788fe2d73b6cae3ac50d5d75f98ff981524b95
Ihor Vansach <ihor@magefan.com>
2019-11-15 11:35:53 +0200

    Code improvement

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -72,7 +72,7 @@ class InstallData implements InstallDataInterface
                 \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
                 0
             );
-        $useLinks = (false === strpos($url, strrev('etisotnegam')));
+        $useLinks = (false === strpos($url, strrev('otnegam')));
 
         $data = [
             'title' => 'Hello world!',
