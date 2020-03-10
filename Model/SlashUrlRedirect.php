206fe803a0f642da20d0ba757b5e5d933eb02790
Ihor Vansach <ihor@magefan.com>
2020-03-10 16:59:44 +0200

    Added to end-slash 301 redirect

---
+++ Model/SlashUrlRedirect.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+namespace Magefan\Blog\Model;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\App\ActionFlag;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class SlashUrlRedirect Model
+ */
+class SlashUrlRedirect
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlInterface;
+
+    /**
+     * @var \Magento\Framework\App\ActionFlag
+     */
+    protected $actionFlag;
+
+    /**
+     * NoSlashUrlRedirect constructor.
+     * @param UrlInterface $urlInterface
+     * @param ActionFlag $actionFlag
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        UrlInterface $urlInterface,
+        ActionFlag $actionFlag,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->actionFlag = $actionFlag;
+        $this->urlInterface = $urlInterface;
+    }
+
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $moduleEnabled = $this->scopeConfig->getValue(Config::XML_PATH_EXTENSION_ENABLED, ScopeInterface::SCOPE_STORE);
+
+        if ($moduleEnabled) {
+            $currentUrl = $this->urlInterface->getCurrentUrl();
+            $result = explode('?', $currentUrl);
+            $result[0] = trim($result[0], '/') . '/';
+            $urlSlash = implode($result, '?');
+
+            if ($urlSlash != $currentUrl) {
+                $controller = $observer->getEvent()->getData('controller_action');
+                if ($controller->getRequest()->isXmlHttpRequest()
+                    || $controller->getRequest()->isPost()
+                ) {
+                    return;
+                }
+                $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH, true);
+                $controller->getResponse()->setRedirect($urlSlash, 301)->sendResponse();
+            }
+        }
+    }
+}
--- Observer/PredispathFrontendBlogActionControllerObserver.php
+++ Observer/PredispathFrontendBlogActionControllerObserver.php
@@ -12,6 +12,7 @@ use Magefan\Blog\Model\Config;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magefan\Blog\Model\NoSlashUrlRedirect;
+use Magefan\Blog\Model\SlashUrlRedirect;
 
 /**
  * Class Predispath Frontend Blog Action Controller Observer
@@ -24,6 +25,11 @@ class PredispathFrontendBlogActionControllerObserver implements ObserverInterfac
     protected $noSlashUrlRedirect;
 
     /**
+     * @var SlashUrlRedirect
+     */
+    protected $slashUrlRedirect;
+
+    /**
      * @var ScopeConfigInterface
      */
     protected $scopeConfig;
@@ -32,13 +38,17 @@ class PredispathFrontendBlogActionControllerObserver implements ObserverInterfac
      * PredispathFrontendBlogActionControllerObserver constructor.
      * @param ScopeConfigInterface $scopeConfig
      * @param NoSlashUrlRedirect $noSlashUrlRedirect
+     * @param SlashUrlRedirect $slashUrlRedirect
      */
     public function __construct(
         ScopeConfigInterface $scopeConfig,
-        NoSlashUrlRedirect $noSlashUrlRedirect
+        NoSlashUrlRedirect $noSlashUrlRedirect,
+        SlashUrlRedirect $slashUrlRedirect = null
     ) {
         $this->scopeConfig = $scopeConfig;
         $this->noSlashUrlRedirect = $noSlashUrlRedirect;
+        $this->slashUrlRedirect = $slashUrlRedirect ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(\Magefan\Blog\Model\SlashUrlRedirect::class);
     }
 
     /**
@@ -47,18 +57,29 @@ class PredispathFrontendBlogActionControllerObserver implements ObserverInterfac
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $redirectToNoSlash = $this->scopeConfig->getValue(
-            Config::XML_PATH_REDIRECT_TO_NO_SLASH,
-            ScopeInterface::SCOPE_STORE
-        );
-
         $advancedPermalinkEnabled =  $this->scopeConfig->getValue(
             Config::XML_PATH_ADVANCED_PERMALINK_ENABLED,
             ScopeInterface::SCOPE_STORE
         );
 
-        if ($redirectToNoSlash && !$advancedPermalinkEnabled) {
+        if ($advancedPermalinkEnabled) {
+            $redirectToNoSlash = $this->scopeConfig->getValue(
+                Config::XML_PATH_REDIRECT_TO_NO_SLASH_BLOG_PLUS,
+                ScopeInterface::SCOPE_STORE
+            );
+        } else {
+            $redirectToNoSlash = $this->scopeConfig->getValue(
+                Config::XML_PATH_REDIRECT_TO_NO_SLASH,
+                ScopeInterface::SCOPE_STORE
+            );
+        }
+
+        if ($redirectToNoSlash) {
             $this->noSlashUrlRedirect->execute($observer);
+        } else {
+            $this->slashUrlRedirect->execute($observer);
         }
     }
+
+
 }
