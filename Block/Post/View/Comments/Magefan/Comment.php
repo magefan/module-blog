c45fba9826a4548c4788262686e6d10fa6552425
Andriy <andriy.martynovskyj@magefan.com>
2020-04-13 16:47:28 +0300

    date frm gmt to local

--- Block/Post/View/Comments/Magefan/Comment.php
+++ Block/Post/View/Comments/Magefan/Comment.php
@@ -11,6 +11,7 @@ namespace Magefan\Blog\Block\Post\View\Comments\Magefan;
 use Magento\Store\Model\ScopeInterface;
 use \Magento\Framework\View\Element\Template;
 use \Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 
 /**
  * Magefan comment block
@@ -20,6 +21,26 @@ use \Magento\Framework\DataObject\IdentityInterface;
  */
 class Comment extends Template implements IdentityInterface
 {
+    /**
+     * @var TimezoneInterface
+     */
+    protected $timezone;
+
+    /**
+     * Comment constructor.
+     * @param Template\Context $context
+     * @param TimezoneInterface $timezone
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        TimezoneInterface $timezone,
+        array $data = []
+    ){
+        $this->timezone = $timezone;
+        parent::__construct($context, $data);
+    }
+
     /**
      * @var array
      */
@@ -88,6 +109,8 @@ class Comment extends Template implements IdentityInterface
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
 
-        return $this->getComment()->getPublishDate($dateFormat);
+        $gmtDate = $this->getComment()->getPublishDate();
+
+        return $this->timezone->date($gmtDate)->format($dateFormat);
     }
 }
