53ea25540d203a5c162a431d897123ef8b80cd26
Magefan <ihoronesuch@gmail.com>
2021-08-12 12:08:00 +0300

    Fix issue with building rss feed url

--- Block/Sidebar/Rss.php
+++ Block/Sidebar/Rss.php
@@ -25,4 +25,40 @@ class Rss extends \Magento\Framework\View\Element\Template
      * @var array
      */
     protected $_months;
+
+    /**
+     * @var \Magefan\Blog\Model\Url
+     */
+    private $blogUrl;
+
+    /**
+     * Generate url by route and parameters
+     *
+     * @param   string $route
+     * @param   array $params
+     * @return  string
+     */
+    public function getUrl($route = '', $params = [])
+    {
+        if ('blog/rss/feed' == $route && empty($params)) {
+             return $this->getBlogUrl()->getUrl('feed', 'rss');
+        }
+        return parent::getUrl($route, $params);
+    }
+
+
+    /**
+     * Retrieve blog url model
+     *
+     * @return  \Magefan\Blog\Model\Url
+     */
+    private function getBlogUrl()
+    {
+        if (null === $this->blogUrl) {
+            $this->blogUrl = \Magento\Framework\App\ObjectManager::getInstance()
+                ->get(\Magefan\Blog\Model\Url::class);
+        }
+
+        return $this->blogUrl;
+    }
 }
