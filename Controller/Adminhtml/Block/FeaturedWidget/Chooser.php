f64a9f983f212b06ecec9fec8414ddc2e2c16703
DenysTsymbal <denys.tsymbal@magefan.com>
2022-06-08 11:52:46 +0300

    Change for using it by two grids

--- Controller/Adminhtml/Block/FeaturedWidget/Chooser.php
+++ Controller/Adminhtml/Block/FeaturedWidget/Chooser.php
@@ -49,10 +49,20 @@ class Chooser extends \Magento\Backend\App\Action
         $layout = $this->layoutFactory->create();
 
         $uniqId = $this->getRequest()->getParam('uniq_id');
-        $pagesGrid = $layout->createBlock(
-            \Magefan\Blog\Block\Adminhtml\Renderer\Grid::class,
-            '',
-        );
+
+        if ($uniqId !== null) {
+            $pagesGrid = $layout->createBlock(
+                \Magefan\Blog\Block\Adminhtml\Widget\FeaturedWidgetChooser::class,
+                '',
+                ['data' => ['id' => $uniqId]]
+            );
+        }
+        else {
+            $pagesGrid = $layout->createBlock(
+                \Magefan\Blog\Block\Adminhtml\Renderer\Grid::class,
+                ''
+            );
+        }
 
         $resultRaw = $this->resultRawFactory->create();
         $resultRaw->setContents($pagesGrid->toHtml());
