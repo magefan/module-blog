2fb33fa29207af6913da3bb57023ac6bc5209020
Magefan <ihoronesuch@gmail.com>
2022-09-05 11:10:57 +0300

    Update UpgradeData.php

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
 
 namespace Magefan\Blog\Setup;
 
