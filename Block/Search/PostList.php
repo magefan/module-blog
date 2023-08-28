f15efde280deb385c506df73f5810d44046753fd
Ihor Vansach <ihor@magefan.com>
2023-08-28 13:54:07 +0300

    Changed comment

--- Block/Search/PostList.php
+++ Block/Search/PostList.php
@@ -62,7 +62,7 @@ class PostList extends \Magefan\Blog\Block\Post\PostList
         $this->_addBreadcrumbs($title, 'blog_search');
         $this->pageConfig->getTitle()->set($title);
         /*
-        $page = $this->_request->getParam(\Magefan\Blog\Block\Post\PostList\Toolbar::PAGE_PARM_NAME);
+        $page = $this->_request->getParam($this->getPageParamName());
         if ($page < 2) {
         */
             $robots = $this->config->getSearchRobots();
