bccd209fe1254e263832a71a46d93dd27f17a7cc
Ihor Mandzyuk <ihor.mandzyuk@magefan.com>
2023-11-08 13:45:39 +0200

    add functionality to comment reply from admin

---
+++ Block/Adminhtml/Comment/ReplyButton.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Block\Adminhtml\Comment;
+
+use Magefan\Community\Block\Adminhtml\Edit\GenericButton;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+/**
+ * Class Reply Button Block
+ */
+class ReplyButton extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Reply'),
+            'on_click' => sprintf("location.href = '%s';", $this->getBackUrl()),
+            'class' => 'reply',
+            'sort_order' => 10
+        ];
+    }
+
+    /**
+     * Get URL for back (reset) button
+     *
+     * @return string
+     */
+    public function getBackUrl()
+    {
+        return $this->getUrl('*/*/addreply/',['id' => $this->getObjectId()]);
+    }
+}
