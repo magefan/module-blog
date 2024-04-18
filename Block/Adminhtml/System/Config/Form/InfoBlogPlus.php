0d4f0f0368265a0b4ded8f4277933e432d1fa9cb
Ivan Hrytsai <ivan.hrytsai@magefan.com>
2024-04-18 11:32:18 +0300

    BlogPlus

---
+++ Block/Adminhtml/System/Config/Form/InfoBlogPlus.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
+ */
+
+declare(strict_types=1);
+
+namespace Magefan\Blog\Block\Adminhtml\System\Config\Form;
+
+class InfoBlogPlus extends InfoPlan
+{
+
+    /**
+     * @return string
+     */
+    protected function getMinPlan(): string
+    {
+        return 'Plus';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSectionId(): string
+    {
+        $sections = json_encode([
+            'mfblog_post_view_related_posts_autorelated_enabled',
+            'mfblog_post_view_related_products_autorelated_enabled',
+            'mfblog_post_view_comments_format_date',
+            'mfblog_sidebar_archive_format_date',
+            'mfblog_post_view_related_products_autorelated_black_words',
+            'mfblog_post_view_related_posts_autorelated_black_words',
+            'mfblog_design',
+            'mfblog_advanced_permalink'
+        ]);
+        return $sections;
+    }
+    protected function getText(): string
+    {
+        return 'This option is available in <strong>Plus</strong> plan only.';
+
+    }
+}
